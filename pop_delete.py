9765e2f320125328ab220df6f288bb569ecd9afa
Michael Kelly <michael@michaelkelly.org>
2011-03-07 23:48:58 -0500

    Add utility for deleting messages from a POP3 server.

---
+++ pop_delete.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -----------------------------------------------------------------
+# pop_delete.py -- Delete messages on a POP server by UID.
+#
+# Useful when you accidentally send yourself an email so malformed that
+# fetchmail won't touch it.
+#
+# Copyright 2011 Michael Kelly (michael@michaelkelly.org)
+#
+# This program is released under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# Mon Mar  7 23:45:24 EST 2011
+# -----------------------------------------------------------------
+
+import poplib
+import logging
+import sys
+from optparse import OptionParser
+
+logging.basicConfig(level=logging.INFO)
+
+parser = OptionParser(usage='Usage: %prog [options] UIDs [...]')
+parser.add_option('-u', dest='username',
+                  help='POP3 username. Required.')
+parser.add_option('-p', dest='password',
+                  help='POP3 password. Required.')
+parser.add_option('-s', dest='server',
+                  help='POP3 server name. Required. Optionally, server:port.')
+
+uids_to_delete = sys.argv[1:]
+
+options, args = parser.parse_args()
+
+if (not args or not options.username or not options.password or not
+    options.server):
+  parser.print_help()
+  sys.exit(2)
+
+uids_to_delete = args
+if ':' in options.server:
+  server, port = options.server.split(':', 1)
+else:
+  server = options.server
+  port = 995
+
+p = poplib.POP3_SSL(server, port)
+p.set_debuglevel(2)
+
+p.user(options.username)
+p.pass_(options.password)
+p.stat()
+
+deleted = 0
+l = p.uidl()
+for message in l[1]:
+  logging.info('Message: %s', message)
+  num, uid = message.split(' ', 2)
+  if uid in uids_to_delete:
+    logging.info('Will delete: %s (%s)', uid, num)
+    p.dele(num)
+    deleted += 1
+
+p.quit()
+logging.info('Done. Deleted %d messages.', deleted)
