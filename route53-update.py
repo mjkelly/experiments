eb84682c278d590f68a6a3692b3183502ec060a7
Michael Kelly <michael@michaelkelly.org>
2012-08-20 22:55:05 -0400

    Add --ip=auto. Clean up help text a little.

--- route53-update.py
+++ route53-update.py
@@ -23,13 +23,17 @@ import optparse
 import sys
 
 parser = optparse.OptionParser()
-parser.add_option('--amz-key-id', dest='key_id', help='Amazon API key ID.')
+parser.add_option('--amz-key-id', dest='key_id',
+                  help='Amazon API key ID. Required.')
 parser.add_option('--amz-key-secret', dest='key_secret',
-                  help='Amazon API key secet value.')
-parser.add_option('--domain', dest='domain', help='Domain name to update.')
+                  help='Amazon API key secet value. Required.')
+parser.add_option('--domain', dest='domain',
+                  help='Domain name to update. Required.')
 parser.add_option('--zone-id', dest='zone_id',
-                  help='Amazon zone ID containing domain name.')
-parser.add_option('--ip', dest='ip', help='New IP for domain name.')
+                  help='Amazon zone ID containing domain name. Required.')
+parser.add_option('--ip', dest='ip', help='New IPv4 for domain name, or '
+                  '"auto" to attempt to auto-detect. "auto" does not work '
+                  'from behind a NAT. Required.')
 opts, _ = parser.parse_args()
 
 def usage():
@@ -37,16 +41,24 @@ def usage():
   parser.print_help()
   sys.exit(2)
 
-def get_time():
+def get_time_and_ip():
   """Gets the current time from amazon servers.
+
+  Also saves the IP address of the socket it uses to make the request. These
+  two bits of functionality are bundled because the IP comes for free from the
+  socket we use to get the date, and we might need the IP later.
 
   Format is RFC 1123.
   http://docs.amazonwebservices.com/Route53/latest/DeveloperGuide/RESTAuthentication.html#StringToSign
+
+  Returns:
+    (date, ipaddr)
   """
   connection = httplib.HTTPSConnection('route53.amazonaws.com')
   connection.request('GET', '/date')
   response = connection.getresponse()
-  return response.getheader('Date')
+  ip = connection.sock.getsockname()[0]
+  return (response.getheader('Date'), ip)
 
 def make_auth(time_str, key_id, secret):
   """Creates an amazon authorization string.
@@ -69,8 +81,8 @@ def get_old_record_values(doc):
   Returns:
     (ip, TTL): the IP and TTL of the existing record
   """
-  # TODO(mjkelly): I think there's some resource-freeing that I'm not doing
-  # here.
+  # TODO(mjkelly): Consider just grabbing the content of <ResourceRecords>
+  # verbatim so we can put it in the delete part of our request.
   xmldoc = libxml2.parseDoc(doc)
   context = xmldoc.xpathNewContext()
   context.xpathRegisterNs('aws',
@@ -84,18 +96,9 @@ def get_old_record_values(doc):
   if not ttls:
     return (None, None)
 
+  # TODO(mjkelly): I think there's some resource-freeing that I'm not doing
+  # here.
   return (values[0].content, ttls[0].content)
-
-if (not opts.key_id or not opts.key_secret or not opts.domain or
-    not opts.zone_id or not opts.ip):
-  usage()
-
-  
-key_id = opts.key_id
-secret = opts.key_secret
-zone_id = opts.zone_id
-domain = opts.domain
-new_ip = opts.ip
 
 # Format string for updating an A record, {name}, from {old_value} with
 # {old_ttl} to {new_value} with {new_ttl}.
@@ -136,7 +139,24 @@ body = """<?xml version="1.0" encoding="UTF-8"?>
    </ChangeBatch>
 </ChangeResourceRecordSetsRequest>
 """
-time_str = get_time()
+
+# Check for all required args.
+if (not opts.key_id or not opts.key_secret or not opts.domain or
+    not opts.zone_id or not opts.ip):
+  usage()
+
+time_str, default_iface_ip = get_time_and_ip()
+key_id = opts.key_id
+secret = opts.key_secret
+zone_id = opts.zone_id
+domain = opts.domain
+if opts.ip == "auto":
+  new_ip = default_iface_ip
+else:
+  new_ip = opts.ip
+
+print 'Will set %s to %s' % (domain, new_ip)
+
 auth = make_auth(time_str, key_id, secret)
 headers = {
   'X-Amz-Date': time_str,
@@ -151,12 +171,11 @@ connection = httplib.HTTPSConnection('route53.amazonaws.com')
 connection.request('GET', get_rrset_path, '', headers)
 response = connection.getresponse()
 old_ip, old_ttl = get_old_record_values(response.read())
-print "old_ip=%s, old_ttl=%s" % (old_ip, old_ttl)
 if old_ip is None:
   raise RuntimeError('Previous IP for A record does not exist or is not parseable.')
 
 if old_ip == new_ip:
-  print 'Old IP %s is same as new IP. We can stop.' % old_ip
+  print 'Old IP %s is same as new IP. Quitting.' % old_ip
   sys.exit(0)
 
 connection = httplib.HTTPSConnection('route53.amazonaws.com')
@@ -167,4 +186,4 @@ change_body = body.format(name=domain,
                           new_ttl=300)
 connection.request('POST', change_rrset_path, change_body, headers)
 response = connection.getresponse()
-print response.read()
+print 'Response: %s' % response.read()
