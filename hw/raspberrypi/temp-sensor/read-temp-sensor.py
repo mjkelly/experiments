e746e5ed8671aa4109352981976eec52ffc1f681
Michael Kelly <michael@michaelkelly.org>
2013-02-20 21:08:05 -0500

    Use rotating file logger, and re-indent consistently.

--- hw/raspberrypi/temp-sensor/read-temp-sensor.py
+++ hw/raspberrypi/temp-sensor/read-temp-sensor.py
@@ -17,6 +17,7 @@ import time
 import os
 import RPi.GPIO as GPIO
 import logging
+import logging.handlers
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
@@ -29,72 +30,78 @@ LOG_FILE = '/var/log/templog'
 CURRENT_TEMP_FILE = '/var/tmp/current_temp_c'
 SLEEP_TIME_S = 30
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+
+# Initialize logging object first so functions can use it.
+logging_handler = logging.handlers.RotatingFileHandler(
+    filename=LOG_FILE, maxBytes=100*1024*1024, backupCount=5)
+logging_handler.setLevel(logging.INFO)
+logging_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s'))
+logger = logging.getLogger('temp-logger')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging_handler)
+
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+  """Reads SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)."""
+  if ((adcnum > 7) or (adcnum < 0)):
+    return -1
+  GPIO.output(cspin, True)
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+  GPIO.output(clockpin, False)  # start clock low
+  GPIO.output(cspin, False)     # bring CS low
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:   
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+  commandout = adcnum
+  commandout |= 0x18  # start bit + single-ended bit
+  commandout <<= 3    # we only need to send 5 bits here
+  for i in range(5):
+    if (commandout & 0x80):
+      GPIO.output(mosipin, True)
+    else:   
+      GPIO.output(mosipin, False)
+    commandout <<= 1
+    GPIO.output(clockpin, True)
+    GPIO.output(clockpin, False)
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+  adcout = 0
+  # read in one empty bit, one null bit and 10 ADC bits
+  for i in range(12):
+    GPIO.output(clockpin, True)
+    GPIO.output(clockpin, False)
+    adcout <<= 1
+    if (GPIO.input(misopin)):
+      adcout |= 0x1
 
-        GPIO.output(cspin, True)
+  GPIO.output(cspin, True)
 
-        adcout /= 2       # first bit is 'null' so drop it
-        return adcout
+  adcout /= 2       # first bit is 'null' so drop it
+  return adcout
 
 recent_temps = []
 
 def output_data(data_read_time, read_adc0, millivolts, temp_C):
-        # remove decimal point from millivolts
-        millivolts = int(millivolts)
+  # remove decimal point from millivolts
+  millivolts = int(millivolts)
 
-        # convert celsius to fahrenheit 
-        temp_F = (temp_C * 9.0 / 5.0) + 32
+  # convert celsius to fahrenheit
+  temp_F = (temp_C * 9.0 / 5.0) + 32
 
-	logging.info("Read: time=%s, read_adc0=%s, mV=%s, "
-		     "temp_C=%.1f, temp_F=%.1f", data_read_time, read_adc0,
-	             millivolts, temp_C, temp_F)
-	
-	recent_temps.append(float(temp_C))
-	while len(recent_temps) > 5:
-		recent_temps.pop(0)
-	logging.info("Last 5 temps (C): %s", recent_temps)
+  logger.info("Read: time=%s, read_adc0=%s, mV=%s, "
+               "temp_C=%.1f, temp_F=%.1f", data_read_time, read_adc0,
+               millivolts, temp_C, temp_F)
 
-	recent_average = sum(recent_temps)/len(recent_temps)
-	logging.info("Recent Average (C): %s", recent_average)
-	try:
-		with open(CURRENT_TEMP_FILE, 'w') as fh:
-			fh.write("%.1f\n" % recent_average)
-	except Exception as e:
-		logging.error("Error writing current temperature: %s", e)
+  recent_temps.append(float(temp_C))
+  while len(recent_temps) > 5:
+    recent_temps.pop(0)
+  logger.info("Last 5 temps (C): %s", recent_temps)
 
+  recent_average = sum(recent_temps)/len(recent_temps)
+  logger.info("Recent Average (C): %s", recent_average)
+  try:
+    with open(CURRENT_TEMP_FILE, 'w') as fh:
+      fh.write("%.1f\n" % recent_average)
+  except Exception as e:
+    logger.error("Error writing current temperature: %s", e)
 
-
-logging.basicConfig(filename=LOG_FILE,
-	format='%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s',
-        level=logging.INFO)
 
 GPIO.setmode(GPIO.BCM)
 
@@ -108,15 +115,15 @@ GPIO.setup(SPICS, GPIO.OUT)
 adcnum = 0
 
 while True:
-        # read the analog pin (temperature sensor LM35)
-        read_adc0 = readadc(adcnum, SPICLK, SPIMOSI, SPIMISO, SPICS)
+  # read the analog pin (temperature sensor LM35)
+  read_adc0 = readadc(adcnum, SPICLK, SPIMOSI, SPIMISO, SPICS)
 
-        # convert analog reading to millivolts = ADC * (3300 / 1024)
-        millivolts = read_adc0 * (3300.0 / 1024.0)
+  # convert analog reading to millivolts = ADC * (3300 / 1024)
+  millivolts = read_adc0 * (3300.0 / 1024.0)
 
-        # 10 mv per degree 
-        temp_C = ((millivolts - 100.0) / 10.0) - 40.0
+  # 10 mv per degree
+  temp_C = ((millivolts - 100.0) / 10.0) - 40.0
 
-	output_data(datetime.datetime.now(), read_adc0, millivolts, temp_C)
+  output_data(datetime.datetime.now(), read_adc0, millivolts, temp_C)
 
-        time.sleep(SLEEP_TIME_S)
+  time.sleep(SLEEP_TIME_S)
