789ff3fb7b1e5dc27dd1d3769bd21d406fa08fbb
Michael Kelly <michael@michaelkelly.org>
2013-03-29 22:55:06 -0400

    Get the entropy calculation right, I think. Take into account that word sampling is random and *unique*.

--- random-phrase.py
+++ random-phrase.py
@@ -38,21 +38,28 @@ with open(dictionary, 'r') as fh:
         continue
       words.append(line)
 
-# Each word is chosen randomly from the eligible candidates.
-bits_per_word = math.log(len(words), 2)
-# Each word in the phrase is independent.
-bits_per_phrase = bits_per_word*num_words
+# random.sample() chooses a random but unique set of words from the candidates,
+# so each word pick is not independent from the others. Because words can't
+# repeat, each additional word in the phrase adds a decreasing amount of
+# entropy. (The different is miniscule, but nonzero.)
+bits_for_first_word = math.log(len(words), 2)
+bits_for_last_word = math.log(len(words) - (num_words - 1), 2)
+bits_per_phrase = sum([math.log(len(words) - n, 2) for n in range(num_words)])
+
 # To conservatively estimate the reduction in entropy from letting the user
 # pick a memorable phrase, we assume an attacker can determine which one was
 # picked.
 bits_per_pick = bits_per_phrase - math.log(num_sentences, 2)
 print("%d possible words (of %d in %s)." % (len(words), total_word_count, dictionary),
       file=sys.stderr)
-print("∴ %.2f bits of entropy per word." % bits_per_word, file=sys.stderr)
-print("%d words per phrase." % num_words, file=sys.stderr)
-print("∴ %.2f bits of entropy per phrase." % bits_per_phrase, file=sys.stderr)
+print("%d words per phrase (random but unique)." % num_words, file=sys.stderr)
+print("∴ %f bits of entropy for first word." % bits_for_first_word,
+      file=sys.stderr)
+print("∴ %f bits of entropy for last word." % bits_for_last_word,
+      file=sys.stderr)
+print("∴ %f bits of entropy per phrase." % bits_per_phrase, file=sys.stderr)
 print("%d phrases to choose from." % num_sentences, file=sys.stderr)
-print("∴ %.2f bits if you pick one phrase from this list." % bits_per_pick,
+print("∴ %f bits if you pick one phrase from this list." % bits_per_pick,
       file=sys.stderr)
 print("---------------------------------------------------", file=sys.stderr)
 
