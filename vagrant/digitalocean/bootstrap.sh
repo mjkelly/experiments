df60280121644f234dcb51438df6e2ed024ee383
Michael Kelly <michael@michaelkelly.org>
2015-01-18 13:05:39 -0500

    Move bootstrap.sh to central place.

--- vagrant/digitalocean/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd $HOME
-sudo apt-get update
-sudo apt-get install -y git
-(test -d config || git clone https://github.com/mjkelly/config.git) && \
-  cd config && ./deploy.sh config && ./deploy.sh packages
