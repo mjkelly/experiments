dfdb9bfc967242adf6339e5c81fb8b69847c5cfc
Michael Kelly <michael@michaelkelly.org>
2015-02-12 00:34:45 -0500

    Remove comment about net/http's FileServer.
    GUESS WHO FIXED IT. (A while ago, actually...)

--- go/webserver-simple.go
+++ go/webserver-simple.go
@@ -1,10 +1,6 @@
 // This is an even simpler webserver that serves a given directory. No
 // authentication. This version uses only
 //
-// Note that http.FileServer is very simple and doesn't really work --
-// filenames with special URL characters in them ('#', '?') will cause broken
-// URLs. You can escape the <a> tag via filenames as well.
-//
 // I run this behind an apache reverse proxy that adds digest authentication
 // and SSL. I use it to serve email attachments that I want to access remotely.
 package main
