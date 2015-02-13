7466ee56c12255d90739b349268fca48debc2332
Michael Kelly <michael@michaelkelly.org>
2015-02-13 16:53:03 -0500

    Adjust prometheus/run.sh for unattended use.
    Remove -ti, add --restart=always.

--- docker/prometheus/run.sh
+++ docker/prometheus/run.sh
@@ -3,7 +3,8 @@
 # This is an example of how to run this docker image. This just maps both data
 # and conf volumes to the docker host. You may wish to pass "-ti" to run
 # interactively.
-docker run -ti \
+docker run \
+  --restart=always \
   -p 0.0.0.0:9090:9090 \
   -v $PWD/data:/data \
   -v $PWD/conf:/conf \
