fcdcf07a75622a61b6e1a4046365ae8f19f03285
Michael Kelly <michael@michaelkelly.org>
2015-02-13 16:57:25 -0500

    Really update prometheus/run.sh for unattended use this time.
    Pass -d.

--- docker/prometheus/run.sh
+++ docker/prometheus/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This is an example of how to run this docker image. This just maps both data
-# and conf volumes to the docker host. You may wish to pass "-ti" to run
-# interactively.
-docker run \
+# and conf volumes to the docker host. You may wish to pass "-ti" instead of
+# "-d" to run interactively.
+docker run -d \
   --restart=always \
   -p 0.0.0.0:9090:9090 \
   -v $PWD/data:/data \
