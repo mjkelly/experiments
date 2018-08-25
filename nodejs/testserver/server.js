2569e2d51f4b8eb7172a0dbabb2ab235e9de9adf
Michael Kelly <michael@michaelkelly.org>
2018-08-25 00:53:28 -0400

    Test node server that doesn't do anything
    This is my first foray into this stuff. But it lints cleanly with xo!

---
+++ nodejs/testserver/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const promclient = require('prom-client');
+
+const port = 8080;
+const app = express();
+const serverName = process.env.server_name || 'UNKNOWN';
+
+const counter = new promclient.Counter({
+  name: 'hit_counter',
+  help: 'Count of hits per endpoint',
+  labelNames: ['path']
+});
+
+app.get('/', (req, res) => {
+  counter.inc({path: '/'});
+  res.send(`Hello, I am ${serverName}`);
+});
+
+app.get('/health', (req, res) => {
+  counter.inc({path: '/health'});
+  res.send('ok\n');
+});
+
+app.get('/vars', (req, res) => {
+  counter.inc({path: '/vars'});
+  res.set('Content-type', 'text/plain');
+  res.send(promclient.register.metrics());
+});
+
+promclient.collectDefaultMetrics({timeout: 5000});
+console.log(`starting on ${port}...`);
+app.listen(port);
