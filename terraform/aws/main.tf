45a202f14d4f9476c965938e91ed2a2eb3d0fdab
Michael Kelly <michael@michaelkelly.org>
2018-09-15 00:58:43 -0400

    Add AWS instance type link

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-b70554c8"
+  // See https://aws.amazon.com/ec2/instance-types/
   instance_type = "t2.micro"
 
   subnet_id     = "subnet-565bc86c"
