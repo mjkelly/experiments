58383349581d5c8c6ed992583813e1f90f7795b9
Michael Kelly <michael@michaelkelly.org>
2019-01-13 21:22:26 -0500

    Run yapf (an auto-formatter) on password scripts

--- random-phrase.py
+++ random-phrase.py
@@ -18,22 +18,36 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Generate random phrases.')
-parser.add_argument('words', type=int, nargs='?', default=5,
-        help='How many words per sentence.')
-parser.add_argument('--sentences', type=int, default=10,
-        help='How many sentences to print at once.')
-parser.add_argument('--simple', default=False, action='store_true',
-        help='Simple mode: Use only lowercase words, without aprostrophes.')
-parser.add_argument('--dictionary', default='/usr/share/dict/words',
-        help='Dictionary file to use. We process the words in this list '
-        'somewhat, so we may not end up using all the words. Check the output '
-        'of the program.')
+parser.add_argument(
+    'words',
+    type=int,
+    nargs='?',
+    default=5,
+    help='How many words per sentence.')
+parser.add_argument(
+    '--sentences',
+    type=int,
+    default=10,
+    help='How many sentences to print at once.')
+parser.add_argument(
+    '--simple',
+    default=False,
+    action='store_true',
+    help='Simple mode: Use only lowercase words, without aprostrophes.')
+parser.add_argument(
+    '--dictionary',
+    default='/usr/share/dict/words',
+    help='Dictionary file to use. We process the words in this list '
+    'somewhat, so we may not end up using all the words. Check the output '
+    'of the program.')
 args = parser.parse_args()
+
 
 def bits2length(bits):
     """Estimates the length of a password that has 'bits' entropy."""
-    chars = 89 # based on our friend, random-string.py
+    chars = 89  # based on our friend, random-string.py
     return round(bits / math.log(chars, 2))
+
 
 num_words = args.words
 num_sentences = args.sentences
@@ -41,20 +55,20 @@ num_sentences = args.sentences
 total_word_count = 0
 words = []
 with open(args.dictionary, 'r') as fh:
-  for line in fh.readlines():
-    total_word_count += 1
-    line = line.strip()
-    if len(line) > 2:
-      if args.simple:
-        if line.lower() != line:
-          continue
-        if "'" in line:
-          continue
-      try:
-        line.encode('ascii')
-      except UnicodeEncodeError:
-        continue
-      words.append(line)
+    for line in fh.readlines():
+        total_word_count += 1
+        line = line.strip()
+        if len(line) > 2:
+            if args.simple:
+                if line.lower() != line:
+                    continue
+                if "'" in line:
+                    continue
+            try:
+                line.encode('ascii')
+            except UnicodeEncodeError:
+                continue
+            words.append(line)
 
 # random.sample() chooses a random but unique set of words from the candidates,
 # so each word pick is not independent from the others. Because words can't
@@ -70,20 +84,27 @@ bits_per_phrase = sum([math.log(len(words) - n, 2) for n in range(num_words)])
 bits_per_pick = bits_per_phrase - math.log(num_sentences, 2)
 
 corresponding_pw = bits2length(bits_per_phrase)
-print("%d possible words (of %d in %s)." % (len(words), total_word_count,
-    args.dictionary), file=sys.stderr)
+print(
+    "%d possible words (of %d in %s)." % (len(words), total_word_count,
+                                          args.dictionary),
+    file=sys.stderr)
 print("%d words per phrase (random but unique)." % num_words, file=sys.stderr)
-print("∴ %f bits of entropy for first word." % bits_for_first_word,
-      file=sys.stderr)
-print("∴ %f bits of entropy for last word." % bits_for_last_word,
-      file=sys.stderr)
+print(
+    "∴ %f bits of entropy for first word." % bits_for_first_word,
+    file=sys.stderr)
+print(
+    "∴ %f bits of entropy for last word." % bits_for_last_word,
+    file=sys.stderr)
 print("∴ %f bits of entropy per phrase." % bits_per_phrase, file=sys.stderr)
-print("(approximately equivalent to %d char random password)" % corresponding_pw, file=sys.stderr)
+print(
+    "(approximately equivalent to %d char random password)" % corresponding_pw,
+    file=sys.stderr)
 print("%d phrases to choose from." % num_sentences, file=sys.stderr)
-print("∴ %f bits if you pick one phrase from this list." % bits_per_pick,
-      file=sys.stderr)
+print(
+    "∴ %f bits if you pick one phrase from this list." % bits_per_pick,
+    file=sys.stderr)
 print("---------------------------------------------------", file=sys.stderr)
 
 r = random.SystemRandom()
 for _ in range(num_sentences):
-  print(*r.sample(words, num_words))
+    print(*r.sample(words, num_words))
--- random-string.py
+++ random-string.py
@@ -23,67 +23,75 @@ import random
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate random printable strings.')
-parser.add_argument('--quiet', default=False, action='store_true',
-                    help='Suppress unnecessary output.')
-parser.add_argument('--alphanum', default=False, action='store_true',
-                    help='Only generate strings from alphanumeric characters. '
-                         'Mutually exclusive with --loweralphanum, --numeric.')
-parser.add_argument('--loweralphanum', default=False, action='store_true',
-                    help='Only generate strings from lowercase alphanumeric characters. '
-                         'Mutually exclusive with --alphanum, --numeric.')
-parser.add_argument('--numeric', default=False, action='store_true',
-                    help='Only generate strings from numbers. '
-                         'Mutually exclusive with --alphanum, --loweralphanum.')
-parser.add_argument('length', default=16, nargs='?', type=int,
-                    help='Number of characters in password.')
+parser = argparse.ArgumentParser(
+    description='Generate random printable strings.')
+parser.add_argument(
+    '--quiet',
+    default=False,
+    action='store_true',
+    help='Suppress unnecessary output.')
+parser.add_argument(
+    '--alphanum',
+    default=False,
+    action='store_true',
+    help='Only generate strings from alphanumeric characters. '
+    'Mutually exclusive with --loweralphanum, --numeric.')
+parser.add_argument(
+    '--loweralphanum',
+    default=False,
+    action='store_true',
+    help='Only generate strings from lowercase alphanumeric characters. '
+    'Mutually exclusive with --alphanum, --numeric.')
+parser.add_argument(
+    '--numeric',
+    default=False,
+    action='store_true',
+    help='Only generate strings from numbers. '
+    'Mutually exclusive with --alphanum, --loweralphanum.')
+parser.add_argument(
+    'length',
+    default=16,
+    nargs='?',
+    type=int,
+    help='Number of characters in password.')
 args = parser.parse_args()
 
-chars_numeric = list(
-  '0123456789'
-)
+chars_numeric = list('0123456789')
 
-chars_loweralpha = list(
-  'abcdefghijklmnopqrstuvwxyz'
-  '0123456789'
-)
+chars_loweralpha = list('abcdefghijklmnopqrstuvwxyz' '0123456789')
 
 # This is included only if --loweralphanum is false.
-chars_upperalpha = list(
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-)
+chars_upperalpha = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 # This is included only if --alphanum and --loweralphanum are false.
 # This list is a little restricted based on what I think will be reasonable to
 # type on most keyboards. I'm skipping: $, which is US-centric. This isn't
 # scientific; it's just guessing, based on me staring at my Thinkpad keyboard.
-chars_symbols = list(
-  '~!@#%^&*()-_=+'
-  '[]{}|;:<>,./?'
-)
+chars_symbols = list('~!@#%^&*()-_=+' '[]{}|;:<>,./?')
 
 if args.loweralphanum and args.alphanum:
-  raise Exception('Cannot specify both --loweralphanum and --alphanum!')
+    raise Exception('Cannot specify both --loweralphanum and --alphanum!')
 
 password_length = args.length
 if args.loweralphanum:
-  chars = chars_loweralpha
+    chars = chars_loweralpha
 elif args.alphanum:
-  chars = chars_loweralpha + chars_upperalpha
+    chars = chars_loweralpha + chars_upperalpha
 elif args.numeric:
-  chars = chars_numeric
+    chars = chars_numeric
 else:
-  chars = chars_loweralpha + chars_upperalpha + chars_symbols
+    chars = chars_loweralpha + chars_upperalpha + chars_symbols
 
 bits_per_char = math.log(len(chars), 2)
 total_bits = bits_per_char * password_length
 
 if not args.quiet:
-  print '%d characters long.' % password_length
-  print 'Choosing from %d characters. %2.3f bits of entropy per character.' % (len(chars), bits_per_char)
-  print '%2.3f total bits of entropy for password.' % total_bits
-  print
-  print 'Password:'
+    print '%d characters long.' % password_length
+    print 'Choosing from %d characters. %2.3f bits of entropy per character.' % (
+        len(chars), bits_per_char)
+    print '%2.3f total bits of entropy for password.' % total_bits
+    print
+    print 'Password:'
 
 r = random.SystemRandom()
 
