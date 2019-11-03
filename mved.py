cf3929b54ed9b2045dfe859338700876b86e3881
Michael Kelly <michael@michaelkelly.org>
2019-11-02 21:38:51 -0400

    Autoformat mved.py (yapf)

--- mved.py
+++ mved.py
@@ -16,118 +16,127 @@ import os
 import tempfile
 import optparse
 
+
 def sorted_file_list(directory, all=False):
-  '''Get a sorted list of the files in the given directory.
+    '''Get a sorted list of the files in the given directory.
 
   Args:
     all: if true, include hidden (dot) files.
   '''
-  files = os.listdir(directory)
+    files = os.listdir(directory)
 
-  if not all:
-    files = [f for f in files if not f.startswith('.')]
-  
-  files.sort()
-  return files
+    if not all:
+        files = [f for f in files if not f.startswith('.')]
+
+    files.sort()
+    return files
+
 
 def get_editor():
-  '''Try to get the user's editor from $EDITOR and $VISUAL.
-  
+    '''Try to get the user's editor from $EDITOR and $VISUAL.
+
   If those aren't set, fall back on 'vi'.
   '''
-  return os.getenv('EDITOR', os.getenv('VISUAL', 'vi'))
+    return os.getenv('EDITOR', os.getenv('VISUAL', 'vi'))
+
 
 def confirm(msg, default=False):
-  '''Prompt the user with message to which they can reply 'y' or 'n'.'''
-  sys.stdout.write(msg + ' [y/N] ')
-  sys.stdout.flush()
-  response = sys.stdin.readline().strip().lower()
-  return response == 'y' or response == 'yes'
+    '''Prompt the user with message to which they can reply 'y' or 'n'.'''
+    sys.stdout.write(msg + ' [y/N] ')
+    sys.stdout.flush()
+    response = sys.stdin.readline().strip().lower()
+    return response == 'y' or response == 'yes'
+
 
 def main(argv):
-  parser = optparse.OptionParser()
-  parser.add_option("-a", action="store_true", dest="list_all",
-    help='List all files (including dotfiles; excluding "." and "..").')
-  parser.set_defaults(list_all=False)
+    parser = optparse.OptionParser()
+    parser.add_option(
+        "-a",
+        action="store_true",
+        dest="list_all",
+        help='List all files (including dotfiles; excluding "." and "..").')
+    parser.set_defaults(list_all=False)
 
-  opts, args = parser.parse_args()
+    opts, args = parser.parse_args()
 
-  cwd = os.getcwd()
+    cwd = os.getcwd()
 
-  files = sorted_file_list(cwd, opts.list_all)
-  editor = get_editor()
-  
-  tmpfd, tmpname = tempfile.mkstemp()
-  for f in files:
-    os.write(tmpfd, bytes(f + "\n", encoding='utf-8'))
-  os.close(tmpfd)
+    files = sorted_file_list(cwd, opts.list_all)
+    editor = get_editor()
 
-  pid = os.fork()
-  if pid == 0:
-    # child
-    try:
-      os.execvp(editor, [editor, tmpname])
-    except RuntimeError as e:
-      print(e)
-      sys.exit(1)
+    tmpfd, tmpname = tempfile.mkstemp()
+    for f in files:
+        os.write(tmpfd, bytes(f + "\n", encoding='utf-8'))
+    os.close(tmpfd)
 
-  # parent (child ends with execvp)
-  w_pid, w_status = os.waitpid(pid, 0)
+    pid = os.fork()
+    if pid == 0:
+        # child
+        try:
+            os.execvp(editor, [editor, tmpname])
+        except RuntimeError as e:
+            print(e)
+            sys.exit(1)
 
-  if w_status != 0:
-    print("%s exited with nonzero status (%d). Aborting." % (editor, w_status))
-    sys.exit(2)
-  
-  tmp = open(tmpname, 'r')
-  new_files = tmp.readlines()
-  if len(new_files) != len(files):
-    print("ERROR: You added or deleted a line. Don't do that. Use blank "
-           "lines to delete files.")
-    sys.exit(2)
+    # parent (child ends with execvp)
+    w_pid, w_status = os.waitpid(pid, 0)
 
-  new_files = [f.rstrip('\n\r') for f in new_files]
+    if w_status != 0:
+        print("%s exited with nonzero status (%d). Aborting." % (editor,
+                                                                 w_status))
+        sys.exit(2)
 
-  renames, deletes = 0, 0
-  for i, new_file in enumerate(new_files):
-    old_file = files[i]
-    if files[i] != new_file:
-      if new_file != '':
-        print("RENAME: '%s' --> '%s'" % (old_file, new_file))
-        renames += 1
-      else:
-        print("DELETE: '%s'" % old_file)
-        deletes += 1
-  tmp.close()
+    tmp = open(tmpname, 'r')
+    new_files = tmp.readlines()
+    if len(new_files) != len(files):
+        print("ERROR: You added or deleted a line. Don't do that. Use blank "
+              "lines to delete files.")
+        sys.exit(2)
 
-  os.unlink(tmpname)
+    new_files = [f.rstrip('\n\r') for f in new_files]
 
-  response = False
-  if renames + deletes > 0:
-    response = confirm(
-      "Rename %d files and delete %d files?" % (renames, deletes),
-      default=False)
-  else:
-    print("No changes.")
-    sys.exit(0)
+    renames, deletes = 0, 0
+    for i, new_file in enumerate(new_files):
+        old_file = files[i]
+        if files[i] != new_file:
+            if new_file != '':
+                print("RENAME: '%s' --> '%s'" % (old_file, new_file))
+                renames += 1
+            else:
+                print("DELETE: '%s'" % old_file)
+                deletes += 1
+    tmp.close()
 
-  if not response:
-    print("No. Aborting.")
-    sys.exit(1)
-  
-  for i, new_file in enumerate(new_files):
-    old_file = files[i]
-    if old_file != new_file:
-      try:
-        if new_file != '':
-          print("RENAME: '%s' --> '%s'" % (old_file, new_file))
-          os.rename(old_file, new_file)
-        else:
-          print("DELETE: '%s'" % old_file)
-          os.unlink(old_file)
-      except OSError as e:
-        print(e)
-  
-  print("Done.")
+    os.unlink(tmpname)
+
+    response = False
+    if renames + deletes > 0:
+        response = confirm(
+            "Rename %d files and delete %d files?" % (renames, deletes),
+            default=False)
+    else:
+        print("No changes.")
+        sys.exit(0)
+
+    if not response:
+        print("No. Aborting.")
+        sys.exit(1)
+
+    for i, new_file in enumerate(new_files):
+        old_file = files[i]
+        if old_file != new_file:
+            try:
+                if new_file != '':
+                    print("RENAME: '%s' --> '%s'" % (old_file, new_file))
+                    os.rename(old_file, new_file)
+                else:
+                    print("DELETE: '%s'" % old_file)
+                    os.unlink(old_file)
+            except OSError as e:
+                print(e)
+
+    print("Done.")
+
 
 if __name__ == '__main__':
-  main(sys.argv)
+    main(sys.argv)
