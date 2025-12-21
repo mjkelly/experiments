4e6582ee4c9d3192c9282426a0ac2d95062cb58b
Michael Kelly <m@michaelkelly.org>
2025-12-21 17:50:42 +0000

    Nicer date formatting; clean up directory scripts

--- docker/docker-tmpl/scripts/port-directory.sh
+++ docker/docker-tmpl/scripts/port-directory.sh
@@ -5,11 +5,11 @@ sudo docker rm -f dir
 sudo docker run \
   --name dir \
   --mount type=bind,src=$HOME/directory.html,dst=/usr/share/nginx/html/index.html \
-  --label http-port=80 \
+  -p 8082:80 \
+  --restart unless-stopped \
+  --label http-port=8082 \
   --label link-container-name=true \
   --label 'traefik.http.routers.dir.tls=true'  \
   --label 'traefik.http.services.dir.loadbalancer.server.port=80' \
-  -p 80:80 \
-  --restart unless-stopped \
   -d \
   nginx
